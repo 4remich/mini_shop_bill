c95b96fd548e2f3eb305fbb90d6bd439377870c5
roman <remich.ua@gmail.com>
2022-12-15 14:13:32 +0200

    add raise methode for price

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ class Generators:
     def __str__(self):
         return f'{self.model} {self.type} {self.power} {self.weight} {self.price}'
 
-
 class Oil:
     def __init__(self, name, volume, price):
         self.name = name
@@ -44,30 +43,30 @@ class Basket:
     def __init__(self, title):
         self.title = title
         self.goods = []
-        # self.total_amount = []
 
     def add_order(self, generator):
         if generator not in self.goods:
             self.goods.append(generator)
-            # self.total_amount.append(float(generator.price))
+
     def add_order(self, oil):
         if oil not in self.goods:
             self.goods.append(oil)
-            # self.total_amount.append(float(oil.price))
+
     def add_order(self, fuel):
         if fuel not in self.goods:
             self.goods.append(fuel)
-            # self.total_amount.append(float(fuel.price))
 
     def total_amount(self):
         sum = 0
         for index, item in enumerate(self.goods):
+            if item.price <= 0:
+                raise ValueError('The price is less than zero. Check the price of the product!')
             sum += item.price
         return sum
 
     def __str__(self):
         return f"{self.title}\n{'-' * 20}\n" + '\n'.join(map(str, self.goods)) + '\n' + f"{'-' * 20}" + '\n' + \
-               str('Total amount:') + f"{self.total_amount()}" + str('UAH')
+        str('Total amount:') + f"{self.total_amount()}" + str('UAH')
 
 
 if __name__ == '__main__':
@@ -76,7 +75,7 @@ if __name__ == '__main__':
     customer_2 = Customer('Petrov', 'Petro', 'petrov***@gmail.com', '+3804455555***')
     customer_3 = Customer('Sydorov', 'Sydor', 'sydorov***@gmail.com', '+380444444***')
 
-    generator_1 = Generators('Werk WPG3600', 'Petrol', '2.8 kW', '45 kg', 27_800.10)
+    generator_1 = Generators('Werk WPG3600', 'Petrol', '2.8 kW', '45 kg', 0)
     generator_2 = Generators('FOGO F3001', 'Petrol', '2.7 kW', '39 kg', 29_500.60)
     generator_3 = Generators('Alimar ALM-B-3000M', 'Petrol', '3.0 kW', '44 kg', 37_700.30)
 
